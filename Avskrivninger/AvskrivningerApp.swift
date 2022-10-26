76e47f5d3de1442ac8636701d6182a335a22cff7
Eirik Østensvik <eirikoo@gmail.com>
2022-10-26 20:21:10 +0200

    Initial Commit

---
+++ Avskrivninger/AvskrivningerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvskrivningerApp.swift
+//  Avskrivninger
+//
+//  Created by Eirik Østensvik on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AvskrivningerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
