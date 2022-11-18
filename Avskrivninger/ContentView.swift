2a91befdb655a964bc49e06f8d75d1d12e4da8ee
Eirik Østensvik <eirikoo@gmail.com>
2022-11-18 10:22:59 +0100

    first one

--- Avskrivninger/ContentView.swift
+++ Avskrivninger/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var prisPerGang:Int = 0
+    @State private var antallGanger:String = "1"
+    @State private var prisen:String = "1"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            //Spacer()
+                Image("fenix7")
+                .resizable()
+                .scaledToFit()
+             Spacer()
+                Image(systemName: "dollarsign.circle")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+            TextField("Hvor mye kostet den", text: $prisen)
+            TextField("Hvor mange ganger har du løpt", text:  $antallGanger)
+             //   .font(.)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                .disableAutocorrection(true)
+            Button("Calculate"){
+                prisPerGang = (Int(prisen) ?? 1)/(Int(antallGanger) ?? 1)
+                
+            }
+            Text("\(prisPerGang)")
+            Spacer()
         }
         .padding()
     }
